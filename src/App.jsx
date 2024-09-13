beb0a814e8d750d40aeaca40e8bfdcc42d278d0d
Abdulloh <email@example.com>
2024-09-13 19:26:30 +0500

    .

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./pages/Header";
+import Footer from "./pages/footer";
+import Hero from "./pages/hero";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
